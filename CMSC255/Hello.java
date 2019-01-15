174eee36be380937e2efd5890c19c63bf2e35ef1
GlennMohler48 <williamsma26@vcu.edu>
2019-01-15 13:45:32 -0500

    Add existing file

---
+++ CMSC255/Hello.java
@@ -0,0 +1,26 @@
+/***********************************************************
+* Hello
+************************************************************
+Prints Hello
+*___________________________________
+Michael Williams
+1.15.19
+CMSC255-002
+*******************************************************/
+
+public class Hello {
+  public static void printHeading() {
+    System.out.println("Michael Williams");
+    System.out.println("Hello World.");
+    System.out.println("Prints Hello");
+    System.out.println("Version 1.15.19");
+    System.out.println("CMSC255-002");
+    System.out.println();
+  }
+
+  public static void main(String [] args){
+    printHeading();
+    System.out.println("Hello");
+
+  }
+}
