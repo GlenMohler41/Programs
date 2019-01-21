4f598970625537fd8ca19ca08925a86623f3ce75
GlennMohler48 <williamsma26@vcu.edu>
2019-01-21 16:40:53 -0500

    Add existing file

---
+++ CMSC255/Project1.java
@@ -0,0 +1,48 @@
+/***********************************************************
+* Project 1
+************************************************************
+Prints My name using * in Code in a Creative way.
+*___________________________________
+Michael Williams
+1.21.19
+CMSC255-002
+*******************************************************/
+
+public class Project1{
+  public static void printHeading() {
+    System.out.println("Michael Williams");
+    System.out.println("Project 1 - Name in Code.");
+    System.out.println("Prints My name using * in Code in a Creative way.");
+    System.out.println("Version 1.21.19");
+    System.out.println("CMSC255-002");
+    System.out.println();
+  }
+  public static void main(String [] args){
+    printHeading();
+    System.out.println("*       *\t");
+    System.out.println("* *   * *\t");
+    System.out.println("*   *   *\t");
+    System.out.println("*       *\t");
+    System.out.println("*       *\t");
+    System.out.println("\n");
+    System.out.println("   *");
+    System.out.println("   *");
+    System.out.println("   *");
+    System.out.println("   *");
+    System.out.println("   *");
+    System.out.println("\n");
+    System.out.println("*     *");
+    System.out.println("*    *");
+    System.out.println("* * *");
+    System.out.println("*    *");
+    System.out.println("*     *");
+    System.out.println("\n");
+    System.out.println("* * * * ");
+    System.out.println("*");
+    System.out.println("* * * * ");
+    System.out.println("*");
+    System.out.println("* * * * ");
+
+
+  }
+}
