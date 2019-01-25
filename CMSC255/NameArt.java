fb8a449ebee011961514df906390759bfd82c09c
GlennMohler48 <williamsma26@vcu.edu>
2019-01-24 19:50:04 -0500

    Add existing file

--- CMSC255/NameArt.java
+++ CMSC255/NameArt.java
@@ -1,10 +1,10 @@
 /***********************************************************
 * Project 1
 ************************************************************
-Prints My name using * in Code in a Creative way.
+Prints My name in Code using printed characters in a Creative way.
 *___________________________________
 Michael Williams
-1.21.19
+1.24.19
 CMSC255-002
 *******************************************************/
 
@@ -12,36 +12,35 @@ public class NameArt {
   public static void printHeading() {
     System.out.println("Michael Williams");
     System.out.println("Project 1 - Name in Code.");
-    System.out.println("Prints My name using * in Code in a Creative way.");
-    System.out.println("Version 1.21.19");
+    System.out.println("Prints My name in Code using printed characters in a Creative way.");
+    System.out.println("Version 1.24.19");
     System.out.println("CMSC255-002");
     System.out.println();
   }
   public static void main(String [] args){
     printHeading();
-    System.out.println("*       *\t");
-    System.out.println("* *   * *\t");
-    System.out.println("*   *   *\t");
-    System.out.println("*       *\t");
-    System.out.println("*       *\t");
-    System.out.println("\n");
-    System.out.println("   *");
-    System.out.println("   *");
-    System.out.println("   *");
-    System.out.println("   *");
-    System.out.println("   *");
-    System.out.println("\n");
-    System.out.println("*     *");
-    System.out.println("*    *");
-    System.out.println("* * *");
-    System.out.println("*    *");
-    System.out.println("*     *");
-    System.out.println("\n");
-    System.out.println("* * * * ");
-    System.out.println("*");
-    System.out.println("* * * * ");
-    System.out.println("*");
-    System.out.println("* * * * ");
+    System.out.print("M       M\t");
+    System.out.print("i\t");
+    System.out.print("k     k\t    ");
+    System.out.println("e e e e ");
+    System.out.print("M M   M M\t");
+    System.out.print("i\t");
+    System.out.print("k    k\t    ");
+    System.out.println("e");
+    System.out.print("M   M   M\t");
+    System.out.print("i\t");
+    System.out.print("k k k\t    ");
+    System.out.println("e e e e ");
+    System.out.print("M       M\t");
+    System.out.print("i\t");
+    System.out.print("k    k\t    ");
+    System.out.println("e");
+    System.out.print("M       M\t");
+    System.out.print("i\t");
+    System.out.print("k     k\t    ");
+    System.out.println("e e e e ");
+
+
 
 
   }
