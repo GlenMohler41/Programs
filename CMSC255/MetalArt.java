336c7e45a6796cbf003a7320cb62a47cc0f7c911
GlennMohler48 <williamsma26@vcu.edu>
2019-02-05 00:49:14 -0500

    Add existing file

--- CMSC255/MetalArt.java
+++ CMSC255/MetalArt.java
@@ -55,11 +55,34 @@ public class MetalArt {
     double prismWidth = scan.nextDouble();
 
     /* The following block of code uses the following equations and the variables
-    entered to calculate the surface area of each of the shapes and the total surface area
-    pyramid SA = B + .5*3*b*l B = .5*b*sqrt((b^2) - (.5*b)^2) l = sqrt((h^2) + (.5*b)^2)
+    entered to calculate the surface area of each of the shapes and the total surface area,
+    as well as the cost of the full structure
+    calculations for each shape are separated by an empty line
+    pyramid SA = B + .5*3*b*l B = .5*b*sqrt((b^2) - (.5*b)^2) l = sqrt((h^2) +second (.5*b)^2)
     Prism SA = 2*l*w + 2*l*h + 2*w*h
+    Total SA = sum of SA of parts
     */
-    
+    double firstSlant = Math.sqrt(Math.pow(firstHeight,2) + Math.pow(.5*firstLength,2));
+    double firstBase = .5*firstLength*Math.sqrt(Math.pow(firstLength,2) - Math.pow(.5*firstLength,2));
+    double firstArea = firstBase + .5*3*firstLength*firstSlant;
+
+    double secondSlant = Math.sqrt(Math.pow(secondHeight,2) + Math.pow(.5*secondLength,2));
+    double secondBase = .5*secondLength*Math.sqrt(Math.pow(secondLength,2) - Math.pow(.5*secondLength,2));
+    double secondArea = secondBase + .5*3*secondLength*secondSlant;
+
+    double prismArea = 2*prismLength*prismWidth + 2*prismLength*prismWidth + 2*prismWidth*prismHeight;
+
+    double totalArea = prismArea + secondArea + firstArea;
+    double totalCost = totalArea*MATERIAL_COST_RATE;
+
+    /* The following block of code outputs the SA of each part to the screen,
+    the total SA, and the cost of the structure. printf is used to maintain two decimal places.
+    */
+    System.out.printf("The Surface Area of the first Pyramid is %,.2f%n", firstArea);
+    System.out.printf("The Surface Area of the second Pyramid is %,.2f%n", secondArea);
+    System.out.printf("The Surface Area of the Prism is %,.2f%n", prismArea);
+    System.out.printf("The Surface Area of %s is %,.2f%n", name, totalArea);
+    System.out.printf("The Cost of %s is %,.2f%n", name, totalCost);
 
 
   }
