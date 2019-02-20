5b182c14f8f1e904ac4a7cac98d8722fb0cefb8d
GlennMohler48 <williamsma26@vcu.edu>
2019-02-20 15:01:50 -0500

    Add existing file

--- CMSC255/MetalChoice.java
+++ CMSC255/MetalChoice.java
@@ -6,7 +6,8 @@
 * material, and number of letters, and outputs
 * the cost of said sculpture. It does this for each sculpture, then displays the total
 * cost. The program continues until the word quit is entered instead of a number of
-* sculpture.
+* sculpture. if gold is chosen as the nameplate material and the name is >10 letters, a $2
+* discount is given
 * ___________________________________
 * Michael Williams
 * 2.20.19
@@ -19,7 +20,7 @@ public class MetalChoice {
   public static void printHeading() {
     System.out.println("Michael Williams");
     System.out.println("MetalChoice");
-    System.out.println("Asks the user for the number of sculptures to be made. Then it reads in the dimensions of a sculpture, the desired material, the nameplate material, and number of letters, and outputs the cost of said sculpture. It does this for each sculpture, then displays the total cost. The program continues until the word quit is entered instead of a number of sculpture.");
+    System.out.println("Asks the user for the number of sculptures to be made. Then it reads in the dimensions of a sculpture, the desired material, the nameplate material, and number of letters, and outputs the cost of said sculpture. It does this for each sculpture, then displays the total cost. The program continues until the word quit is entered instead of a number of sculpture. if gold is chosen as the nameplate material and the name is >10 letters, a $2 discount is given");
     System.out.println("Version 2.20.19");
     System.out.println("CMSC255-002");
     System.out.println();
@@ -30,25 +31,62 @@ public class MetalChoice {
     /* The following line creates the scanner that will be used to take inputs.*/
     Scanner scan = new Scanner(System.in);
 
-    // The following line sets as a constant the cost of material
+    // The following lines set as a constant the cost of material
     final double MATERIAL_COST_RATE_ALUMINUM = 1.50;
     final double MATERIAL_COST_RATE_STEEL = 2.85;
     final double MATERIAL_COST_RATE_TITANIUM = 4.50;
 
+    // The following lines set as a constant the cost of the nameplate
+    final double NAMEPLATE_COST_BRONZE = 8.00;
+    final double NAMEPLATE_COST_SILVER = 12.50;
+    final double NAMEPLATE_COST_GOLD = 19.99;
 
-    /* The following lines instantiate stringSculptures, and starts a while loop
+
+    //The following lines initialize important variables
+    double finalCost = 0;
+    double costRate = 0;
+    double costNameplate = 0;
+    /* The following lines instantiate stringSculptures, and start a while loop
     stringSculptures is read in from input, if it is quit, the loop and the program is ended
     if it isn't,the numSculptures is parsed from the string as an int. */
     String stringSculptures="";
 
     while (true){
+      System.out.println("Please enter the number of desired sculpture or enter quit to stop the program.");
       stringSculptures = scan.next();
       if (stringSculptures.equals("quit")) {
+        System.out.println("Goodbye!");
         return;
       }
       int numSculptures = Integer.parseInt(stringSculptures);
+
+      //the following line sets finalCost to 0 in order to reset the cost
+      finalCost = 0;
       while (numSculptures>0){
 
+        // The following lines ask the user to enter the  desired sculpture material and changes the costRate accordingly
+        System.out.println("Please enter the desired material for your sculpture");
+        String material = scan.next();
+        material = material.toLowerCase();
+        if (material.equals("aluminum")==true){
+          costRate = MATERIAL_COST_RATE_ALUMINUM;
+        } else if (material.equals("steel")==true){
+          costRate = MATERIAL_COST_RATE_STEEL;
+        } else if (material.equals("titanium")==true){
+          costRate = MATERIAL_COST_RATE_TITANIUM;
+        }
+
+        // The following lines ask the user to enter the  desired nameplate material and changes the costRate accordingly
+        System.out.println("Please enter the desired nameplate material for your sculpture");
+        String nameplate = scan.nextLine();
+        nameplate = nameplate.toLowerCase();
+        if (nameplate.equals("bronze")==true){
+          costNameplate = NAMEPLATE_COST_BRONZE;
+        } else if (nameplate.equals("silver plated")==true){
+          costNameplate = NAMEPLATE_COST_SILVER;
+        } else if (nameplate.equals("gold")==true){
+          costNameplate = NAMEPLATE_COST_GOLD;
+        }
 
         /* The following section asks the user for the name of the sculpture,
         the length of the first pyramid's base, the height of the first pyramid,
@@ -92,7 +130,9 @@ public class MetalChoice {
         double prismArea = 2*prismLength*prismWidth + 2*prismLength*prismWidth + 2*prismWidth*prismHeight;
 
         double totalArea = prismArea + secondArea + firstArea;
-        double totalCost = totalArea*MATERIAL_COST_RATE;
+        double totalCost = totalArea*costRate;
+        totalCost = totalCost + costNameplate;
+
 
         /* The following block of code outputs the SA of each part to the screen,
         the total SA, and the cost of the structure. printf is used to maintain two decimal places.
@@ -105,7 +145,9 @@ public class MetalChoice {
 
         // the following line decrements the numSculptures counter by 1
         numSculptures = numSculptures - 1;
+        finalCost = finalCost + totalCost;
       }
+      System.out.printf("The total Cost of all of the sculptures is %,.2f%n", finalCost);
     }
 
   }
